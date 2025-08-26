978b860a0d9cc12905eda1b57d7c366ce5f0caaa
Luvvbinaa <shabrina_shabrina_34rpl@student.smktelkom-mlg.sch.id>
2025-08-26 14:23:49 +0700

    Add files via upload

---
+++ SavingsAccount2.java
@@ -0,0 +1,17 @@
+public class SavingsAccount2 {
+   public double balance;
+   public double interestRate;
+   public String name;
+
+   public void displayCustomer(){
+    System.out.println("Customer : " + name );
+
+    int balance = 1000;
+    String name = "Damien";
+    int balance1 = 1000;
+    String name1 = "Damien";
+    int balance2 = 2000;
+    String name2 = "Bill";
+
+   }
+}
