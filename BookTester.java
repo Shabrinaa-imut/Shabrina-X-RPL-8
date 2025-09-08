57d139412fa54450630676525a58bd3c4484f4d2
Luvvbinaa <shabrina_shabrina_34rpl@student.smktelkom-mlg.sch.id>
2025-09-08 16:42:17 +0700

    Add files via upload

---
+++ BookTester.java
@@ -0,0 +1,7 @@
+public class BookTester {
+    public static void main(String[] args) {
+        //membuat object dari class Book
+        Book luvvbinaa = new Book(title:"Katakan sayang",year:2021,price:99000);
+        luvvbinaa.display();
+    }
+}
